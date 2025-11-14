ab780e788c8accd8d2d0d829152317ec4c56f952
danielvici123 <danielvici123@proton.me>
2025-11-14 21:27:05 +0100

    first site, and add navbar

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLinkActive,
+    RouterLink,
+    NgOptimizedImage
+  ],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+
+}
---
+++ src/app/sites/startpage/startpage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-startpage',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './startpage.html',
+  styleUrl: './startpage.css',
+})
+export class Startpage {
+
+}
