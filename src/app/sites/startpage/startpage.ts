485f54029a607cc349a7358a2d09b53b81607561
danielvici123 <danielvici123@proton.me>
2025-12-06 21:37:23 +0100

    Paths changed to plural, tool descriptions moved to separate file

--- src/app/sites/startpage/startpage.ts
+++ src/app/sites/startpage/startpage.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
 import { CustomTitle } from '../../components/custom-title/custom-title';
 import { CustomCard } from '../../components/custom-card/custom-card';
 import { ReactiveFormsModule } from '@angular/forms';
+import  features from './features.json';
 
 @Component({
   selector: 'app-startpage',
@@ -11,42 +11,5 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [CustomTitle, CustomCard, ReactiveFormsModule],
 })
 export class Startpage {
-  features = [
-    {
-      name: 'Generators',
-      enabled: true,
-      tools: [
-        {
-          name: 'Password Generator',
-          description: 'Create secure passwords with custom options',
-          link: '/generator/password',
-          enabled: true,
-        },
-        {
-          name: 'QR Code Generator',
-          description: 'Generate and download QR codes for any text or URL',
-          link: '/generator/qrcode',
-          enabled: true,
-        },
-      ],
-    },
-    {
-      name: 'Tests',
-      enabled: true,
-      tools: [
-        {
-          name: 'Test 1',
-          description: 'This is a just test description.',
-          link: '/test',
-          enabled: true,
-        },
-        {
-          name: 'Test 2',
-          description: 'This is a just test description.',
-          link: '/test',
-          enabled: false,
-        },
-      ],
-    },
-  ];
+  features: any[] = features;
 }
